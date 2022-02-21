0f10ff6199d7336e8cf2d7518bcb5cf8d70ea9cf
Kazeem <kezyolanipekun@gmail.com>
2022-02-21 03:00:25 +0100

    Improve design spec

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ router.group('/api', rt => {
     rt.use(CheckoutRoute);
     rt.use(CustomerRoute);
     rt.use(InvoiceRoute);
+    rt.use(ResponseHandler.globalError)
 });
 
 module.exports = router;
